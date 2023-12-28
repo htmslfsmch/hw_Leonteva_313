a31b027c6f9b0f55669d8fc7b4a7dcf847440bd7
leonteva.ekaterina <katya_leont@mail.ru>
2023-12-28 20:17:32 +0300

    hw_7

--- hw_7.py
+++ hw_7.py
@@ -1,33 +1,20 @@
-class Book:
-    def __init__(self, name: str, author: str, year: int, pages: int):
-        self.name = name
-        self.author = author
-        self.year = year
+class Book():
+    def __init__(self,title,author,year,pages):
+        self.title=title
+        self.author=author
+        self.year=year
+        self.pages=pages
+    def info(self):
+        print('Название:',self.title)
+        print("Автор:",self.author)
+        print('Год издания:',self.year)
+        print('Колличество страниц:',self.pages)
+    def new_pages(self, pages):
         self.pages = pages
-
-    def print_info(self):
-        print(
-            f"О книге {self.name}:\t Автор: {self.author}, Год издания: {self.year}, Количество страниц: {self.pages}")
-
-    def set_pages(self, new_pages: int):
-        self.pages = new_pages
-
     def __str__(self):
-        return f"{self.name}, {self.author}, {self.year}, {self.pages}"
-
-
-book1 = Book("Бойцовский клуб", "Чак Паланик", 1996, 256)
-book2 = Book("Яма", "Александр Куприн", 1915, 416)
-
-book1.print_info()
-book2.print_info()
-
-book1.set_pages(100)
-book2.set_pages(100)
-
-print("\nУстановили новые значения для pages")
-
-book1.print_info()
-book2.print_info()
-
-print("\n", book1, book2, sep="\n")
+        return f'self.title,self.author,self.year,self.pages'
+example_book=Book('Борцовский клуб','Чак Паланик', 1996, 256)
+example_book.info()
+example_book.new_pages()
+example_book.info()
+print(example_book)

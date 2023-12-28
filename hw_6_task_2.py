5742882707579ce6d8536b73f4c52c7cd320df5e
leonteva.ekaterina <katya_leont@mail.ru>
2023-12-28 19:46:13 +0300

    hw_6

---
+++ hw_6_task_2.py
@@ -0,0 +1,9 @@
+import re
+
+words = input()
+
+def find_words(text):
+    result = re.findall(r"\b[а-яёА-ЯЁa-zA-Z-]+", text)
+    return result
+
+print(len(find_words(words)))
---
+++ hw_6_task_3.py
@@ -0,0 +1,10 @@
+import re
+
+text_to_correct = input()
+
+change_to = "(TBD)"
+
+result = re.sub(pattern="([0-1][0-9]|[2][0-3])(:[0-5][0-9]){1,2}",
+                repl=change_to, string=text_to_correct)
+
+print(result)
